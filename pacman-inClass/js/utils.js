b6475ad00cacb03e9ec9537cfc21ad7d557368b8
EranAAA <32829466+EranAAA@users.noreply.github.com>
2022-03-22 20:29:34 +0200

    Add files via upload

---
+++ pacman-inClass/js/utils.js
@@ -0,0 +1,53 @@
+
+function printMat(mat, selector) {
+  var strHTML = '<table border="0"><tbody>';
+  for (var i = 0; i < mat.length; i++) {
+    strHTML += '<tr>';
+    for (var j = 0; j < mat[0].length; j++) {
+      var cell = mat[i][j];
+      var className = 'cell cell-' + i + '-' + j;
+      strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+    }
+    strHTML += '</tr>'
+  }
+  strHTML += '</tbody></table>';
+  var elContainer = document.querySelector(selector);
+  elContainer.innerHTML = strHTML;
+}
+
+function renderCell(location, value) {
+  // Select the elCell and set the value
+  var elCell = document.querySelector(`.cell-${location.i}-${location.j}`);
+  elCell.innerHTML = value;
+}
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return String(color);
+}
+
+function getEmptyCellsBoard(board) {
+  var emptyCellsBoard = []
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === EMPTY) {
+        emptyCellsBoard.push({ i: i, j: j })
+      }
+    }
+  }
+  return emptyCellsBoard
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
